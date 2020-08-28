f3d2bc1a8d0bbbdc37aa22398a6eac606b7d7569
JanardhanSingh98 <janardhansingh1998@gmail.com>
2020-08-28 23:23:28 +0530

    login works

--- src/views/AppBar.vue
+++ src/views/AppBar.vue
@@ -10,11 +10,63 @@
         <b-navbar-nav class="ml-auto">
           <b-nav-form>
             <b-button size="sm" class="my-1 mx-1 my-sm-0" type="submit" to="/">Home</b-button>
-            <b-button size="sm" class="my-1 mx-1 my-sm-0" type="submit" to="/login">Login</b-button>
-            <b-button size="sm" class="my-1 mx-1 my-sm-0" type="submit" to="/signup">Signup</b-button>
+            <b-button
+              v-if="auth==''"
+              size="sm"
+              class="my-1 mx-1 my-sm-0"
+              type="submit"
+              to="/login"
+            >Login</b-button>
+            <b-button
+              v-if="auth==''"
+              size="sm"
+              class="my-1 mx-1 my-sm-0"
+              type="submit"
+              to="/signup"
+            >Signup</b-button>
+            <b-button
+              v-if="auth=='loggedin'"
+              size="sm"
+              class="my-1 mx-1 my-sm-0"
+              type="submit"
+              to="/profile"
+            >Profile</b-button>
+            <b-button
+              v-if="auth=='loggedin'"
+              size="sm"
+              class="my-1 mx-1 my-sm-0"
+              type="submit"
+              to="/signup"
+              @click="logout"
+            >Logout</b-button>
           </b-nav-form>
         </b-navbar-nav>
       </b-collapse>
     </b-navbar>
   </div>
 </template>
+
+<script>
+import EventBus from "./EventBus";
+
+export default {
+  data() {
+    return {
+      auth: "",
+      user: "",
+    };
+  },
+
+  methods: {
+    logout() {
+      localStorage.removeItem("usertoken");
+    },
+  },
+
+  mounted() {
+    EventBus.$on("logged-in", (status) => {
+      this.auth = status;
+    });
+  },
+};
+</script>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,25 +1,32 @@
 <template>
   <section class="form-elegant">
     <mdb-row class="m-3 p-3 d-flex justify-content-center">
-      <mdb-col md="5">
+      <mdb-col md="6" sm="8" lg="4">
         <mdb-card>
           <mdb-card-body class="mx-4">
-            <div class="text-center">
-              <h3 class="dark-grey-text mb-5">
-                <strong>LogIn</strong>
-              </h3>
-            </div>
-            <mdb-input label="Your email" type="email" />
-            <mdb-input label="Your password" type="password" containerClass="mb-0" />
-            <p class="font-small blue-text d-flex justify-content-end pb-3">Forgot Password?</p>
-            <div class="text-center mb-3">
-              <mdb-btn type="button" gradient="blue" rounded class="btn-block z-depth-1a">Sign in</mdb-btn>
-            </div>
+            <form v-on:submit.prevent="login">
+              <div class="text-center">
+                <h3 class="dark-grey-text mb-5">
+                  <strong>LogIn</strong>
+                </h3>
+              </div>
+              <div class="grey-text form-group">
+                <mdb-input v-model="email" label="Your email" group type="email" required />
+                <mdb-input v-model="password" label="Your password" required group type="password" />
+              </div>
+              <div class="text-center mb-3 form-group">
+                <mdb-btn
+                  type="submit"
+                  gradient="blue"
+                  class="btn-lg btn-primary btn-block z-depth-1a"
+                >LogIn</mdb-btn>
+              </div>
+            </form>
           </mdb-card-body>
           <mdb-modal-footer class="mx-5 pt-3 mb-1">
             <p class="font-small grey-text d-flex justify-content-end">
               Not a member?
-              <a class="blue-text ml-1" to="/signup">Sign Up</a>
+              <a class="blue-text ml-1" @click="route">Sign Up</a>
             </p>
           </mdb-modal-footer>
         </mdb-card>
@@ -30,8 +37,7 @@
 
 
 <script>
-// eslint-disable-next-line no-unused-vars
-import axios from "axios";
+import EventBus from "./EventBus";
 
 export default {
   data() {
@@ -39,6 +45,33 @@ export default {
       email: "",
       password: "",
     };
+  },
+
+  methods: {
+    route() {
+      this.$router.push("/signup");
+    },
+
+    login() {
+      this.axios
+        .post("http://127.0.0.1:5000/users/login", {
+          email: this.email,
+          password: this.password,
+        })
+        .then((response) => {
+          localStorage.setItem("usertoken", response.data);
+          this.email = "";
+          this.password = "";
+          this.$router.push("/profile");
+        })
+        .catch((error) => {
+          console.log(error + "errer hai");
+        });
+      this.emitMethod();
+    },
+    emitMethod() {
+      EventBus.$emit("logged-in", "loggedin");
+    },
   },
 };
 </script>
